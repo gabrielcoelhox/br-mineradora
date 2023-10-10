93fc06f663b68830a2dd41f4373f1cd16ab6ae73
Gabriel Coelho <gabriel.coelhox@gmail.com>
2023-10-09 21:33:32 -0300

    Adding first structure of project: Entities, repository and dto

---
+++ cotacao/src/main/java/com/br/mineradora/entity/QuotationEntity.java
@@ -0,0 +1,29 @@
+package com.br.mineradora.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="quotation")
+@Data
+@NoArgsConstructor
+public class QuotationEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Date date;
+
+    @Column(name="currency_price")
+    private BigDecimal currencyPrice;
+
+    @Column(name="percentage_change")
+    private String percentageChange;
+
+    private String pairCurrency;
+}
---
+++ cotacao/src/main/java/com/br/mineradora/repository/QuotationRepository.java
@@ -0,0 +1,10 @@
+package com.br.mineradora.repository;
+
+import com.br.mineradora.entity.QuotationEntity;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class QuotationRepository implements PanacheRepository<QuotationEntity> {
+
+}
